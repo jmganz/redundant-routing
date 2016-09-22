d627c6cf4c8937c72d0c84502ce758152251e44a
jmganz <jmganz@ucdavis.edu>
2016-09-22 02:08:46 -0700

    Modified plots to be solid lines instead of individual datapoints. Traffic is now In + Out for each interface. Handled outliers so that plots focus on the majority of data. TODO: resolve issue in cleanOutput.py that removes some interface status events from certain CSV files

--- plotThroughput.py
+++ plotThroughput.py
@@ -7,6 +7,7 @@ import glob
 import math
 import numpy
 from array import array
+import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
@@ -16,7 +17,6 @@ files = sorted(glob.glob('*.csv'))
 
 for file in files:
   date = "-".join(file.split("-")[2:]).split(".")[0]
-  print file
   with open (file, "r") as data:
     dataPoints = []
     timestamp = []
@@ -24,6 +24,7 @@ for file in files:
     interfaceActivity = [[],[]]
     MbitOut = []
     MbitIn = []
+    traffic = []
     packetsOut = []
     packetsIn = []
     errorsIn = []
@@ -36,11 +37,11 @@ for file in files:
       else:
         dataPoints.append(line.split(";"))
     for i in range(len(dataPoints)):
-      #print dataPoints[i]
       timestamp.append(dataPoints[i][0])
       interfaceField.append(dataPoints[i][1])
       MbitOut.append(float(dataPoints[i][2]) * 8.0 / 1000000.0)
       MbitIn.append(float(dataPoints[i][3]) * 8.0 / 1000000.0)
+      traffic.append(float((float(dataPoints[i][2])) * 8.0 / 1000000.0) + ((float(dataPoints[i][2])) * 8.0 / 1000000.0))
       packetsOut.append(float(dataPoints[i][7]))
       packetsIn.append(float(dataPoints[i][8]))
       errorsIn.append(int(dataPoints[i][14]))
@@ -51,6 +52,7 @@ for file in files:
   avgTimestamp = [[] for _ in xrange(len(ifaces))]
   avgMbitOut = [[] for _ in xrange(len(ifaces))]
   avgMbitIn = [[] for _ in xrange(len(ifaces))]
+  avgTraffic = [[] for _ in xrange(len(ifaces))]
   avgPacketsOut = [[] for _ in xrange(len(ifaces))]
   avgPacketsIn = [[] for _ in xrange(len(ifaces))]
   # Average the data over 10 samples
@@ -65,12 +67,14 @@ for file in files:
         break
     sumMbitOut = [[] for _ in xrange(len(ifaces))]
     sumMbitIn = [[] for _ in xrange(len(ifaces))]
+    sumTraffic = [[] for _ in xrange(len(ifaces))]
     sumPacketsOut = [[] for _ in xrange(len(ifaces))]
     sumPacketsIn = [[] for _ in xrange(len(ifaces))]
     for k in range(j + 1):
       whichIface = k % len(ifaces)
       sumMbitOut[whichIface].append(MbitOut[i+k])
       sumMbitIn[whichIface].append(MbitIn[i+k])
+      sumTraffic[whichIface].append(traffic[i+k])
       sumPacketsOut[whichIface].append(packetsOut[i+k])
       sumPacketsIn[whichIface].append(packetsIn[i+k])
     for m in range(len(ifaces)):
@@ -78,6 +82,7 @@ for file in files:
         avgTimestamp[m].append(timestamp[i])
         avgMbitOut[m].append(sum(sumMbitOut[m]) / len(sumMbitOut[m]))
         avgMbitIn[m].append(sum(sumMbitIn[m]) / len(sumMbitIn[m]))
+        avgTraffic[m].append(sum(sumTraffic[m]) / len(sumTraffic[m]))
         avgPacketsOut[m].append(sum(sumPacketsOut[m]) / len(sumPacketsOut[m]))
         avgPacketsIn[m].append(sum(sumPacketsIn[m]) / len(sumPacketsIn[m]))
     skip = j
@@ -85,38 +90,41 @@ for file in files:
   for index in range(len(interfaceActivity[0])):
     interfaceActivity[0][index] = (interfaceActivity[0][index] / (10 * len(ifaces)))
 
+  color = ['b', 'r', 'g', 'm', 'k', 'c', 'y']
+
+  plt.figure(num=None, figsize=(16, 12), dpi=90, facecolor='w', edgecolor='k')
+  ax = plt.subplot(111)
+  graphTraffic = []
+  upperLimit = -20
   for index in range(len(ifaces)):
-    plt.figure(num=None, figsize=(16, 12), dpi=90, facecolor='w', edgecolor='k')
+    if ('total' == ifaces[index]) or ('lo' == ifaces[index]):
+      continue
+    graphTraffic.append(mlines.Line2D(range(len(avgTimestamp[index])), avgTraffic[index], color=color[index], label=ifaces[index]))
+    ax.add_line(mlines.Line2D(range(len(avgTimestamp[index])), avgTraffic[index], color=color[index], label=ifaces[index]))
 
-    ax = plt.subplot(111)
-    graphMbitOut = ax.scatter(range(len(avgTimestamp[index])), avgMbitOut[index], s=20, c='b', alpha=0.8, lw=0, label='Traffic Out')
-    graphMbitIn = ax.scatter(range(len(avgTimestamp[index])), avgMbitIn[index], s=20, c='r', alpha=0.8, lw=0, label='Traffic In')
+    avgTraffic[index].sort(reverse=True)
+    for i in range(len(avgTraffic[index])):
+      if avgTraffic[index][i] < 2 * avgTraffic[index][i+1]:
+        goodMax = avgTraffic[index][i] # eliminate outliers
+        break
+    if upperLimit < goodMax:
+      upperLimit = goodMax
+  plt.legend(handles=graphTraffic, loc=2)
 
-    ax.legend(handles=[graphMbitOut, graphMbitIn], loc=2)
+  plt.xlim([-20, max([len(avgTimestamp[i]) for i in range(len(avgTimestamp))]) + 20])
+  plt.ylim([-5, upperLimit + 20])
+  position = upperLimit / 5
+  for i in range(len(interfaceActivity[0])):
+    position += upperLimit / 10
+    plt.axvline(interfaceActivity[0][i])
+    ax.annotate(interfaceActivity[1][i], xy=(0, 0), xytext=((interfaceActivity[0][i] + 5), position))
 
-    
-    upperLimit = max(max(avgMbitOut[index]), max(avgMbitIn[index]))
-    upperLimit = 200
-    plt.xlim([-20, len(avgTimestamp[index]) + 20])
-    plt.ylim([-5, upperLimit + 20])
+  plt.xlabel('Time (s)')
+  plt.ylabel('Throughput (Mbps)')
+  plt.title('Throughput On ' + str(file.split("-")[0]) + ' During File Transfer')
 
-    position = upperLimit / 3
-    for i in range(len(interfaceActivity[0])):
-      position += 10
-      #print interfaceActivity[0][i]
-      plt.axvline(interfaceActivity[0][i])
-      ax.annotate(interfaceActivity[1][i], xy=(0, 0), xytext=((interfaceActivity[0][i] + 5), position))
-
-    plt.xlabel('Time (s)')
-    plt.ylabel('Throughput (Mbps)')
-    if ('total' == ifaces[index]):
-      plt.title('Throughput During File Transfer On All Interfaces')
-    else:
-      plt.title('Throughput During File Transfer On ' + ifaces[index])
-
-    saveLocation = 'plots/' + file.split("-")[0] + '-' + ifaces[index] + '-' + date + '.pdf'
-    saveLocation2 = "png/" + file.split("-")[0] + '-' + ifaces[index] + '-' + date + '.png'
-    plt.savefig(saveLocation, bbox_inches='tight')
-    plt.savefig(saveLocation2, bbox_inches='tight')
-    #plt.show()
-    plt.close()
+  saveLocation = 'plots/' + file.split("-")[0] + '-' + date + '.pdf'
+  saveLocation2 = "png/" + file.split("-")[0] + '-' + date + '.png'
+  plt.savefig(saveLocation, bbox_inches='tight')
+  plt.savefig(saveLocation2, bbox_inches='tight')
+  plt.close()
