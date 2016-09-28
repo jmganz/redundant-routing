418f9e2bb80c4552f126969e4c9926a3af43c380
jmganz <jmganz@ucdavis.edu>
2016-09-28 02:54:10 -0700

    I think it's very close. The vertical lines to denote network interface activity are near the correct location on the x-axis, but they are still a bit off. I need to look into this.

--- plotThroughput.py
+++ plotThroughput.py
@@ -13,8 +13,7 @@ import matplotlib.patches as mpatches
 
 # Collect the data from the given file
 
-#files = sorted(glob.glob('*.csv'))
-files = ['clientEdge-throughput-09-20-16_02:16.csv']
+files = sorted(glob.glob('*.csv'))
 
 for file in files:
   date = "-".join(file.split("-")[2:]).split(".")[0]
@@ -25,7 +24,7 @@ for file in files:
     ifaces = []
     for line in data:
       if "timestamp" in line or "down" in line or "back" in line:
-        interfaceActivity[0].append(len(dataPoints))
+        interfaceActivity[0].append(int(dataPoints[len(dataPoints) - 1][0]))
         interfaceActivity[1].append(line[1:].split('at')[0])
       else:
         dataPoints.append(line.split(';'))
@@ -43,16 +42,10 @@ for file in files:
     errorsOut = [[] for _ in xrange(len(ifaces))]
     drift = 0
     index = 0
-    #shift = 0
     while index < (len(dataPoints) - drift):
       adjustedIndex = index + drift
       whichInterface = adjustedIndex % len(ifaces)
       while (dataPoints[adjustedIndex][1] != ifaces[whichInterface]):
-        #print 'mismatch! (' + dataPoints[adjustedIndex][1] + ' != ' + ifaces[whichInterface] + ') adding entry'
-        #whichInterface = (whichInterface - 1) % len(ifaces)
-        #datalist = [dataPoints[i][0], ifaces[whichInterface], '0.00', '0.00', '0.00', '0', '0', '0.00', '0.00', '0.00', '0', '0', '0.00', '0.00', '0', '0\n']
-        #print datalist
-        #print 'storing values in ' + ifaces[whichInterface]
         timestamp[whichInterface].append(dataPoints[i][0])
         MbitOut[whichInterface].append(0)
         MbitIn[whichInterface].append(0)
@@ -61,21 +54,8 @@ for file in files:
         packetsIn[whichInterface].append(0)
         errorsIn[whichInterface].append(0)
         errorsOut[whichInterface].append(0)
-        #ifaces = ifaces[1:] + ifaces[:1] # SHIFT DATA
-        #print 'now we have ' + ifaces[whichInterface]
-        #shift = 1
-        #whichInterface = i % len(ifaces)
         drift = drift + 1
-        #ifaces = ifaces[1:] + ifaces[:1] # SHIFT DATA
         whichInterface = (index + drift) % len(ifaces)
-      #print 'done'
-      #if shift:
-      #  whichInterface = (index + drift) % len(ifaces)
-      #  whichInterface = i % len(ifaces)
-      #  ifaces = ifaces[1:] + ifaces[:1] # SHIFT DATA
-      #  shift = 0
-      #print dataPoints[i]
-      #print 'storing values in ' + ifaces[whichInterface]
       timestamp[whichInterface].append(dataPoints[adjustedIndex][0])
       MbitOut[whichInterface].append(float(dataPoints[adjustedIndex][2]) * 8.0 / 1000000.0)
       MbitIn[whichInterface].append(float(dataPoints[adjustedIndex][3]) * 8.0 / 1000000.0)
@@ -86,14 +66,8 @@ for file in files:
       errorsOut[whichInterface].append(int(dataPoints[adjustedIndex][15]))
       index = index + 1
 
-  '''
-    PROGRAM MUST DETECT WHEN INTERFACES GO DOWN
-    THIS IS WHAT CAUSES GRAPHS TO BE INACCURATE
-  '''
-  while (ifaces[0] != interfaceField[0]):
-    ifaces = ifaces[1:] + ifaces[:1]
-  for i in range(len(ifaces)):
-    print ifaces[i] + ':\n\n' + '\n'.join([str(trafficElement) for trafficElement in MbitOut[i]]) + '\n\n'
+    while (ifaces[0] != interfaceField[0]):
+      ifaces = ifaces[1:] + ifaces[:1]
 
   avgTimestamp = [[] for _ in xrange(len(ifaces))]
   avgMbitOut = [[] for _ in xrange(len(ifaces))]
@@ -110,49 +84,34 @@ for file in files:
     if skip:
       skip -= 1
       continue
-    #print range(10 * len(ifaces))
     for j in range(10 * len(ifaces)):
-      #print str(j) + ' and ' + str(i+j)
       if timestamp[0][i] != timestamp[0][i+j]:
         j -= 1
         break
-    #print 'done'
     sumMbitOut = [[] for _ in xrange(len(ifaces))]
     sumMbitIn = [[] for _ in xrange(len(ifaces))]
     sumTraffic = [[] for _ in xrange(len(ifaces))]
     sumPacketsOut = [[] for _ in xrange(len(ifaces))]
     sumPacketsIn = [[] for _ in xrange(len(ifaces))]
-    #print 'j is ' + str(range(j + 1))
-    #print len(MbitOut[i % len(ifaces)])
     for k in range(j + 1):
-      #print 'k is ' + str(k)
       whichIface = (i + k) % len(ifaces)
-      #print (i + k)
-      #print interfaceField[i+k] + ': ' + str(MbitOut[i+k])
       sumMbitOut[whichIface].append(MbitOut[whichIface][i+k])
       sumMbitIn[whichIface].append(MbitIn[whichIface][i+k])
       sumTraffic[whichIface].append(traffic[whichIface][i+k])
       sumPacketsOut[whichIface].append(packetsOut[whichIface][i+k])
       sumPacketsIn[whichIface].append(packetsIn[whichIface][i+k])
-    #for p in range(len(ifaces)):
-    #  print ifaces[p] + ': ' + str(sumMbitOut[p])
     for m in range(len(ifaces)):
-      #print ifaces[m] + ': ' + str(sumMbitOut[m])
       if (len(sumMbitOut[m]) > 0):
-        #print 
         avgTimestamp[m].append(timestamp[m][i])
         avgMbitOut[m].append(sum(sumMbitOut[m]) / len(sumMbitOut[m]))
         avgMbitIn[m].append(sum(sumMbitIn[m]) / len(sumMbitIn[m]))
         avgTraffic[m].append(sum(sumTraffic[m]) / len(sumTraffic[m]))
         avgPacketsOut[m].append(sum(sumPacketsOut[m]) / len(sumPacketsOut[m]))
         avgPacketsIn[m].append(sum(sumPacketsIn[m]) / len(sumPacketsIn[m]))
+    for intCount in range(len(interfaceActivity[0])):
+      if (int(interfaceActivity[0][intCount]) == int(timestamp[0][i])):
+        interfaceActivity[0][intCount] = i / 10
     skip = j
-
-  #for m in range(len(ifaces)):
-  #  print ifaces[m] + ': ' + str(avgMbitOut[m])
-
-  for index in range(len(interfaceActivity[0])):
-    interfaceActivity[0][index] = (interfaceActivity[0][index] / (10 * len(ifaces)))
 
   color = ['b', 'r', 'g', 'm', 'k', 'c', 'y']
 
@@ -168,7 +127,6 @@ for file in files:
 
     avgMbitOut[index].sort(reverse=True)
     goodMax = 0
-    #print len(avgMbitOut[index])
     for i in range(len(avgMbitOut[index])):
       if avgMbitOut[index][i] <= 2 * avgMbitOut[index][i+5]:
         goodMax = avgMbitOut[index][i] # eliminate outliers
@@ -187,13 +145,10 @@ for file in files:
 
   plt.xlabel('Time (s)')
   plt.ylabel('Throughput (Mbps)')
-  #plt.title('Throughput On ' + str(file.split("-")[0]) + ' During File Transfer')
-  plt.title('Throughput On test During File Transfer')
+  plt.title('Throughput On ' + str(file.split("-")[0]) + ' During File Transfer')
 
-  #saveLocation = 'plots/' + file.split("-")[0] + '-' + date + '.pdf'
-  #saveLocation2 = "png/" + file.split("-")[0] + '-' + date + '.png'
-  saveLocation = 'plots/' + file + '.pdf'
-  saveLocation2 = "png/" + file + '.png'
+  saveLocation = 'plots/' + file.split("-")[0] + '-' + date + '.pdf'
+  saveLocation2 = "png/" + file.split("-")[0] + '-' + date + '.png'
   plt.savefig(saveLocation, bbox_inches='tight')
   plt.savefig(saveLocation2, bbox_inches='tight')
   plt.close()
